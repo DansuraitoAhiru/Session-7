80df782dcd68b417f4efb11f3aedf866521260e6
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-30 23:16:34 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session7_BTVN05.cpp

--- SS7/Vũ Quang Việt_B25DTCN217_IT102_Session7_BTVN05.cpp
+++ SS7/Vũ Quang Việt_B25DTCN217_IT102_Session7_BTVN05.cpp
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <math.h>
-int main(){
-	int n,m,b=0;
-	printf("Enter n: ");
-	scanf("%d", &n);
-	int original=n;
-	while(n>0){
-		int temp=n%10;
-		b=b+temp*temp*temp;
-		n/=10;
-		}
-	if(b==original){
-		printf("%d la so Amstrong \n", original);
-	}else{
-	    printf("%d ko phai la so Amstrong \n", original);
-	}
+int main() {
+    int n, original, count = 0, sum = 0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    original = n;
+
+    int temp = n;
+    while (temp != 0) {
+        temp /= 10;
+        count++;
+    }
+    
+    temp = n;
+    while (temp != 0) {
+        int digit = temp % 10;
+        sum += pow(digit, count);
+        temp /= 10;
+    }
+
+    if (sum == original) {
+        printf("%d la so Armstrong\n", original);
+    } else {
+        printf("%d khong phai la so Armstrong\n", original);
+    }
+
+    return 0;
 }
